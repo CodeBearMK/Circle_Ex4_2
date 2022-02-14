14fc5cf3f3152f8864040ebe839ca89e5f432d09
cat32tw <mike1993xd@gmail.com>
2022-02-14 20:26:55 +0800

    第四章習題2

---
+++ Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Ex4_2
+{
+    internal class Circle
+    {
+        public double radius { get; set; }
+
+        public double CalArea()
+        {
+            return Math.Round(radius * radius * 3.14,2);
+        }
+
+        public double CalCircumference()
+        {
+            return Math.Round(radius * 2 * 3.14,2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Circle_Ex4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            Circle circle = new Circle();
+            Console.Write("輸入圓形半徑:");
+            r = double.Parse(Console.ReadLine());
+            circle.radius = r;
+            if (r < 10)
+            {
+                circle.radius = 10;
+            }
+            else if (r > 20)
+            {
+                circle.radius = 20;
+            }
+            Console.WriteLine("使用半徑 = {0}", circle.radius);
+            Console.WriteLine("圓周長 = {0}", circle.CalCircumference());
+            Console.WriteLine("圓面積 = {0}", circle.CalArea());
+            Console.Read();
+        }
+    }
+}
